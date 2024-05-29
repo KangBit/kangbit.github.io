6e3df48d97a49209d9c577cdb3ff8b20288c567e
KangBit <kangbit@kakao.com>
2024-05-29 15:38:50 +0900

    Post 목록 페이지 추가

--- .vitepress/config/nav.js
+++ .vitepress/config/nav.js
@@ -1,30 +1,30 @@
-export default [
-  { text: "Home", link: "/" },
-  {
-    text: "Links",
-    items: [
-      {
-        items: [
-          { text: "JS", link: "/posts/links/javascript" },
-          { text: "CSS", link: "/posts/links/typescript" },
-        ],
-      },
-      {
-        items: [
-          { text: "VUE", link: "/posts/links/vue" },
-          { text: "REACT", link: "/posts/links/react" },
-        ],
-      },
-      {
-        items: [
-          { text: "IOS", link: "/posts/links/ios" },
-        ],
-      },
-      {
-        items: [
-          { text: "etc", link: "/posts/links/etc" },
-        ],
-      },
-    ],
-  },
-];
+const homeMenu = { text: "Home", link: "/" };
+const postMenu = {
+  text: "Post",
+  link: "/posts",
+};
+const linkMenu = {
+  text: "Links",
+  items: [
+    {
+      items: [
+        { text: "JS", link: "/posts/links/javascript" },
+        { text: "CSS", link: "/posts/links/typescript" },
+      ],
+    },
+    {
+      items: [
+        { text: "VUE", link: "/posts/links/vue" },
+        { text: "REACT", link: "/posts/links/react" },
+      ],
+    },
+    {
+      items: [{ text: "IOS", link: "/posts/links/ios" }],
+    },
+    {
+      items: [{ text: "etc", link: "/posts/links/etc" }],
+    },
+  ],
+};
+
+export default [homeMenu, postMenu, linkMenu];
---
+++ pages/posts.md
@@ -0,0 +1,12 @@
+---
+titleTemplate: Post 목록
+layout: home
+
+hero:
+  name: ""
+
+features:
+  - title: VitePress + Github Pages로 블로그 만들기
+    details: VitePress를 이용해 블로그 홈페이지를 생성하는 방법을 소개합니다.
+    link: "/posts/vitepress/why-vitepress"
+---
