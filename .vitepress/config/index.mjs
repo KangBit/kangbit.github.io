ca9d8916d0ea5c55b9eb9d104ca12a95bc2c95e3
Kangbit <kang.b.c.1208@gmail.com>
2026-04-24 16:27:35 +0900

    feat: add Clarity tracking script to VitePress configuration

--- .vitepress/config/index.mjs
+++ .vitepress/config/index.mjs
@@ -140,6 +140,15 @@ export default defineConfig({
       gtag('config', 'G-P9FSHHKTBN');`,
     ],
     [
+      "script",
+      {},
+      `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "wglb9vbp1y");`,
+    ],
+    [
       "meta",
       {
         name: "naver-site-verification",
