31e4dd5b7576d80c39a1faa7f88c559f6c0024fb
Kangbit <kang.b.c.1208@gmail.com>
2026-04-29 16:08:22 +0900

    fix: update ESLint label in navigation for clarity

--- .vitepress/config/nav.js
+++ .vitepress/config/nav.js
@@ -13,7 +13,7 @@ const devMenu = {
     {
       items: [
         { text: "Whats New In", link: "/posts/updates" },
-        { text: "eslint/js", link: "/posts/eslint/settings" },
+        { text: "ESLint", link: "/posts/eslint/settings" },
       ],
     },
   ],
